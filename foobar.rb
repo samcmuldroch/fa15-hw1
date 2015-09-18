4e5b89ec361701ad6a27337714e3879ef60012bf
vagrant <vagrant@rails-dev-box>
2015-09-18 06:56:59 +0000

    Completed HW1

--- foobar.rb
+++ foobar.rb
@@ -1,3 +1,18 @@
 class Foobar
-  # Q4 CODE HERE
+  def Foobar.baz(a)
+  	b = []
+  	for i in a
+  		i = i.to_i
+  		i = i + 2
+  		if i % 2 == 0 and i < 10
+  			b << i
+  		end
+  	end
+  	b = b.uniq
+  	sum = 0
+  	for i in b
+  		sum = sum + i
+  	end
+  	return sum
+  end
 end
--- hw1.rb
+++ hw1.rb
@@ -1,13 +1,20 @@
 def squared_sum(a, b)
-  # Q1 CODE HERE
+  c = a + b
+  c = c * c
+  return c
 end
 
 def sort_array_plus_one(a)
-  # Q2 CODE HERE
+   a = a.sort
+   b = []
+   for i in a
+    b<<(i+1)
+   end
+   return b
 end
 
 def combine_name(first_name, last_name)
-  # Q3 CODE HERE
+  first_name + " " + last_name
 end
 
 def blockin_time(a)
